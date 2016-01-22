626df356ac465aec6e2483fd2c7df9c3a12835d5
Erik Berdonces Bonelo <sturgelose@gmail.com>
2016-01-22 12:31:45 +0100

    Update setup.sh
    Add libpam-python to the needed packages

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # install pip
 # installs python_pam.so in /lib/security
-apt-get install python-dev python-pip -y
+apt-get install python-dev python-pip libpam-python -y
 
 # clone and install python package dependencies
 cd ~
