6b8ad3abf5cf33ddd1f4bc30a4e5c56ffdb0a149
Erik Berdonces Bonelo <sturgelose@gmail.com>
2017-05-12 11:52:22 +0200

    Allow google to choose xPra installation

--- setup.sh
+++ setup.sh
@@ -6,23 +6,37 @@ apt-get update
 apt-get install python-dev python-pip libpam-python git -y
 
 # Clone and install python package dependencies
-cd ~
-mkdir cyclone-pam && cd cyclone-pam
-git clone https://github.com/cyclone-project/cyclone-python-pam.git .
+cd /tmp
+git clone https://github.com/cyclone-project/cyclone-python-pam.git . && cd cyclone-python-pam
 git checkout V2
 pip install -r requirements.pip
 
 # Install python script and config
 cp usr/local/bin/cyclone_pam.py /usr/local/bin/cyclone_pam.py
-cp --parents etc/cyclone/* /
-
-# Update ssh PAM config
+cp etc/cyclone/cyclone.conf /etc/cyclone/cyclone.conf
+cp etc/cyclone/authenticated.html /etc/cyclone/authenticated.html
 cp etc/pam.d/sshd /etc/pam.d/sshd
-
-# Update sshd configuration and restart service
 cp etc/ssh/sshd_config /etc/ssh/sshd_config
 service ssh restart
 
-# Clean installation files
-cd ~ && rm -rf cyclone-pam
+## INSTALL XPRA
 
+# Install only if we have the XPRA_INSTALL ENV variable
+if [ -z "$XPRA_INSTALL" ]; then
+    # Install xPra latest version from WinSwitch repo
+    curl http://winswitch.org/gpg.asc | apt-key add -
+    echo "deb http://winswitch.org/ xenial main" > /etc/apt/sources.list.d/winswitch.list
+    apt-get install -y software-properties-common
+    add-apt-repository universe
+    apt-get update
+    apt-get install -y xpra
+    # Install xFce
+    apt-get install -y xfce4
+
+    # Start xPra at start and execute it now (need to update to use random local internal port!)
+    cp etc/rc.local /etc/rc.local
+    chmod +x /etc/rc.local
+fi
+
+# Clean up installation files
+#cd /tmp && rm -rf cyclone-cyclone-pam
